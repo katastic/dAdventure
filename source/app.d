aa0aa74c1ccc95361e7340b4be5ec5cbb7ee616d
katastic <ckatko@gmail.com>
2024-07-03 03:14:10 -0500

    stuff

--- source/app.d
+++ source/app.d
@@ -90,43 +90,51 @@ struct bitmap {
 struct ALLEGRO_FILE {} // https://liballeg.org/a5docs/trunk/file.html#allegro_file
 //dfmt on
 
-struct AllegroFileBuffer {
+struct AllegroFileBuffer
+{
 	ALLEGRO_FILE* f;
 	bool isLoaded = false;
 
-	this(string filepath) {
+	this(string filepath)
+	{
 		load(filepath);
 	}
 
-	void load(string filepath) { // should load() be apart of the constructor?
+	void load(string filepath)
+	{ // should load() be apart of the constructor?
 		import std.file : read;
 		import std.string : toStringz;
 
 		// f = al_fopen(filepath.toStringz(), "rb");
 	}
 
-	bitmap get() {
+	bitmap get()
+	{
 		//return = al_load_bitmap_f(f, NULL);
 		return bitmap();
 	}
 }
 
-struct FileBuffer {
+struct FileBuffer
+{
 	string path;
 	ubyte[] data;
 	bool isLoaded = false;
 
-	this(ubyte[] _data) {
+	this(ubyte[] _data)
+	{
 		data = _data;
 	}
 
-	void load(string filepath) { // should load() be apart of the constructor?
+	void load(string filepath)
+	{ // should load() be apart of the constructor?
 		import std.file : read;
 
 		data = cast(ubyte[]) read(filepath); // can fire exception
 	}
 
-	void get() {
+	void get()
+	{
 		if (!isLoaded)
 			throw new Exception("shit");
 	}
@@ -134,26 +142,31 @@ struct FileBuffer {
 // And if so, do we want a non-templated version for simple/quicker cases?
 // do we want a separate version for ALLEGRO_FILE's, or merge them together somehow?
 
-struct bitsmap { // too close to bitmap? better name?
+struct bitsmap
+{ // too close to bitmap? better name?
 	uint w, h;
 	bool[] data;
 
-	this(AllegroFileBuffer f) {
+	this(AllegroFileBuffer f)
+	{
 		load(f);
 	}
 
-	void load(FileBuffer f) {
+	void load(FileBuffer f)
+	{
 		// todo
 	}
 
-	void load(AllegroFileBuffer f) {
+	void load(AllegroFileBuffer f)
+	{
 		bitmap b = f.get;
 		for (int i = 0; i < b.w; i++)
 			for (int j = 0; j < b.h; j++)
 				set(i, j, (b.get(i, j).r > .1)); // set true/false based on R component                          
 	}
 
-	void set(uint _i, uint _j, bool value) {
+	void set(uint _i, uint _j, bool value)
+	{
 		assert(_i < w && _j < h);
 		data[_i + _j * w] = value;
 	}
@@ -161,17 +174,20 @@ struct bitsmap { // too close to bitmap? better name?
 	bool get(uint _i, uint _j) => data[_i + _j * w];
 }
 
-class PixelMap {
+class PixelMap
+{
 	bitsmap collisionMap; // does this even need to be separate?
 	int nLayers = 0; // do we even NEED multiple layers?
 	// i mean if we're using it for visual layers in ADDITION to collision, then sure why not.
 
-	this(string mapConfigFilePath) {
+	this(string mapConfigFilePath)
+	{
 		loadConfig(mapConfigFilePath);
 		enforce(nLayers > 0, "Map file did not contain any layers");
 	}
 
-	final void loadConfig(string filepath) {
+	final void loadConfig(string filepath)
+	{
 		import std.file, std.conv; //import TOML;
 		string s = to!string(read(filepath));
 
@@ -182,47 +198,56 @@ class PixelMap {
         enforce("Invalid map config file");+/
 	}
 
-	void loadLayer(string filepath) { // in case we have multiple layers?
+	void loadLayer(string filepath)
+	{ // in case we have multiple layers?
 		import std.file;
 
 		collisionMap.load(AllegroFileBuffer(filepath));
 	}
 
-	bool isValidAt(uint _i, uint _j) {
+	bool isValidAt(uint _i, uint _j)
+	{
 		return collisionMap.get(_i, _j);
 	}
 }
 
-class PixelMapCollider {
+class PixelMapCollider
+{
 	PixelMap* _map;
-	this(PixelMap* map) {
+	this(PixelMap* map)
+	{
 		_map = map;
 	}
 }
 
-interface Component { // certain ones don't have an ondraw or ontick. Does that matter? Does either (all have, or we only add to specifics) matter?
+interface Component
+{ // certain ones don't have an ondraw or ontick. Does that matter? Does either (all have, or we only add to specifics) matter?
 	void onTick();
 	bool onDraw(Viewport v);
 } // if they don't want to respond to the event, who cares? Just don't define anything to happen.
 // it's not like we have 5,000 optional events and only attaching to one.
 
-class PhysicsCom : Component {
+class PhysicsCom : Component
+{
 	pair pos;
 	pair vel;
 	uint screenIndex; /// Which screen are we on?
-	this() {
+	this()
+	{
 	}
 
 	void onTick() => noop;
 	bool onDraw(Viewport v) => noopDraw;
 }
 
-class GraphicsCom : Component {
+class GraphicsCom : Component
+{
 	PhysicsCom phy;
 	bitmap[] bmps;
 	bitmap bmp;
 
-	this(PhysicsCom _phy) {
+	this(PhysicsCom _phy)
+	{
 		phy = _phy;
 	}
 
@@ -234,92 +259,111 @@ class GraphicsCom : Component {
 	int nDirections;
 	DIR dir;
 
-	void frameNext() {
+	void frameNext()
+	{
 		currentFrame++;
 		if (currentFrame == nFrames)
 			currentFrame = 0;
 	}
 
-	void setDirection(DIR d) {
+	void setDirection(DIR d)
+	{
 		assert(d < nDirections);
 		dir = d;
 	}
 }
 
-void alDrawBitmap(bitmap b, pair pos, Viewport v) {
+void alDrawBitmap(bitmap b, pair pos, Viewport v)
+{
 	// do stuff
 }
 
-void alDrawText(string text, pair pos, Viewport v) {
+void alDrawText(string text, pair pos, Viewport v)
+{
 }
 
-class SpriteGraphicsCom : GraphicsCom {
-	this(PhysicsCom _phy) {
+class SpriteGraphicsCom : GraphicsCom
+{
+	this(PhysicsCom _phy)
+	{
 		super(_phy);
 	}
 
-	override bool onDraw(Viewport v) {
+	override bool onDraw(Viewport v)
+	{
 		alDrawBitmap(bmp, phy.pos, v);
 		return 0;
 	}
 }
 
-class AnimatedGraphicsCom : GraphicsCom {
-	this(PhysicsCom _phy) {
+class AnimatedGraphicsCom : GraphicsCom
+{
+	this(PhysicsCom _phy)
+	{
 		super(_phy);
 	}
 
-	override bool onDraw(Viewport v) {
+	override bool onDraw(Viewport v)
+	{
 		alDrawBitmap(bmps[currentFrame], phy.pos, v);
 		return 0;
 	}
 }
 
-class AudioCom : Component {
+class AudioCom : Component
+{
 	void onTick() => noop;
 	bool onDraw(Viewport v) => noopDraw;
 }
 
-class NetworkCom : Component {
+class NetworkCom : Component
+{
 	void onTick() => noop;
 	bool onDraw(Viewport v) => noopDraw;
 }
 
-class AICom : Component {
+class AICom : Component
+{
 	void onTick() => noop;
 	bool onDraw(Viewport v) => noopDraw;
 }
 
-class BaseObject { // common functionality between units (players) and items
+class BaseObject
+{ // common functionality between units (players) and items
 	// but wait, what about the fact we're using COMPONENTS?
 }
 
 // these are one-off components, on the otherhand, they are independant for task-based composition
 // ideally they'd be batches of units? Meh, it'll be fine.
 // only particle engine would even remotely hit performance limitations.
-class Unit : BaseObject {
+class Unit : BaseObject
+{
 	AICom ai;
 	PhysicsCom physics;
 	AudioCom audio;
 	GraphicsCom gfx;
 	NetworkCom net; // when/where to call ontick for this? Queue up messages? [respond] to nets, [run] code, [send] to net? One authoritave server though.
 
-	this() {
+	this()
+	{
 	}
 
-	void onTick() {
+	void onTick()
+	{
 		ai.onTick(); // do we react after physics, or physics after we make decisions?
 		physics.onTick();
 		audio.onTick(); // can probably do this async/parallel with gfx
 		//gfx.onTick(); // should do nothing, right?
 	}
 
-	bool onDraw(Viewport v) {
+	bool onDraw(Viewport v)
+	{
 		return gfx.onDraw(v);
 	}
 }
 
-enum DIR {
+enum DIR
+{
 	UP = 0, // 1 = IDLE
 	DOWN,
 	LEFT,
@@ -330,24 +374,32 @@ enum DIR {
 	DOWNRIGHT // 8
 }
 
-class PlayerControlledAI : AICom {
+class PlayerControlledAI : AICom
+{
 } // we can also easily replace this with a REPLAY for player actions!
 
-class ReplayPlayerControlledAI : AICom {
+class ReplayPlayerControlledAI : AICom
+{
 }
 
-class GroundPhysicsCom : PhysicsCom {
-	this() {
+class GroundPhysicsCom : PhysicsCom
+{
+	this()
+	{
 	}
 } // check floor layer, wall layer? + screen borders
 
-class FloatingPhysicsCom : PhysicsCom {
-	this() {
+class FloatingPhysicsCom : PhysicsCom
+{
+	this()
+	{
 	}
 } // basically just checks borders of screen / nogo zone for a screen.
 
-class PlayerDude : Unit {
-	this() {
+class PlayerDude : Unit
+{
+	this()
+	{
 		ai = new PlayerControlledAI();
 		physics = new GroundPhysicsCom();
 		gfx = new GraphicsCom(physics);
@@ -357,7 +409,8 @@ class PlayerDude : Unit {
 	// don't have to override onTick or onDraw. Components are called the same. Customize the components.
 }
 
-class InventoryHotbarDialog { /// Visual and mouse elements for hotbar
+class InventoryHotbarDialog
+{ /// Visual and mouse elements for hotbar
 	rect dim;
 	float cellHeight = 32;
 	float cellWidth = 32;
@@ -365,38 +418,46 @@ class InventoryHotbarDialog { /// Visual and mouse elements for hotbar
 	InventoryHotbar hotbar;
 
 	void onTick() => noop;
-	bool onDraw(Viewport v) {
-		foreach (i; hotbar.items) {
+	bool onDraw(Viewport v)
+	{
+		foreach (i; hotbar.items)
+		{
 			i.onDraw(v);
 		}
 		return 0;
 	}
 }
 
-class InventoryHotbar {
+class InventoryHotbar
+{
 	ItemInstance[] items;
 	int nHotbarCells;
 	int cellIdx;
 
-	void actionSelection() { /// Press activate on the active hotbar item
+	void actionSelection()
+	{ /// Press activate on the active hotbar item
 		items[cellIdx].actionActivate();
 	}
 
-	void actionSelection(int specificIndex) { /// Press activate on a hotbar item
+	void actionSelection(int specificIndex)
+	{ /// Press activate on a hotbar item
 		enforce(specificIndex >= 0 && specificIndex < nHotbarCells);
 		items[specificIndex].actionActivate();
 	}
 
-	void actionLeft() {
+	void actionLeft()
+	{
 		cellIdx = clampLow(cellIdx - 1, 0);
 	}
 
-	void actionRight() {
+	void actionRight()
+	{
 		cellIdx = clampHigh(cellIdx + 1, nHotbarCells - 1);
 	}
 }
 
-class ItemInstance { // use a BaseObject?
+class ItemInstance
+{ // use a BaseObject?
 	Item itemType;
 
 	//bool isHidden; // needed? If inside inventory but not inventory isn't open?
@@ -411,14 +472,16 @@ class ItemInstance { // use a BaseObject?
 	bool drawInside(Viewport v) => noopDraw; /// for hotbar, inventory, etc.
 	bool drawOutside(Viewport v) => noopDraw; /// for active canvas gameplay
 
-	void actionActivate() {
+	void actionActivate()
+	{
 		enforce(itemType !is null);
 		itemType.actionActivate();
 	}
 }
 
 // type vs instance?
-class Item {
+class Item
+{
 	string name;
 	string description;
 	Item[] decaysTo; /// Item(s) this decays to (if we use this mechanic. Whether through damage, time, uses, or recycling somehow)
@@ -432,8 +495,10 @@ class Item {
 	bool onDraw(Viewport v) => noopDraw;
 }
 
-class Spacesuit : Item {
-	this() {
+class Spacesuit : Item
+{
+	this()
+	{
 		name = "Spacesuit";
 		description = "";
 		recyclesTo = [];
@@ -441,8 +506,10 @@ class Spacesuit : Item {
 	}
 }
 
-class OxygenTank : Item {
-	this() {
+class OxygenTank : Item
+{
+	this()
+	{
 		name = "Oxygen Tank";
 		description = "";
 		recyclesTo = []; // [Metal]. this is a TYPE. Do we have an instance of each one for pointing to, or do we use compile-time reflection?
@@ -450,36 +517,44 @@ class OxygenTank : Item {
 	}
 }
 
-class Metal : Item {
-	this() {
+class Metal : Item
+{
+	this()
+	{
 		name = "Metal";
 		description = "";
 	}
 }
 
-union MessagePayload {
+union MessagePayload
+{
 	char[32] strVal;
 	float floatVal;
 	int intVal;
 }
 
-class Message {
+class Message
+{
 	string channel; // fixed string max for performance?
 	MessagePayload payload;
 }
 
-class MessageHandler { // internal use only
+class MessageHandler
+{ // internal use only
 	string[Message[]] buffer;
 
 	void send(T)(string channel, T val) => noop;
 
-	void onTick() {
-		foreach (b; buffer) {
+	void onTick()
+	{
+		foreach (b; buffer)
+		{
 		}
 	}
 }
 
-class ProgressBar {
+class ProgressBar
+{
 	bitmap backgroundTex;
 	bitmap filledTex;
 	bitmap unfilledTex;
@@ -487,7 +562,8 @@ class ProgressBar {
 	minMaxValue val = minMaxValue(0f, 1500f, 1500f);
 
 	void onTick() => noop;
-	bool onDraw(Viewport v) {
+	bool onDraw(Viewport v)
+	{
 		float fullWidth = dim.w;
 		float coefficent = (val.value / val.max);
 		float filledWidth = fullWidth * coefficent;
@@ -500,28 +576,33 @@ class ProgressBar {
 	}
 }
 
-class DialogProgressBar {
+class DialogProgressBar
+{
 	string title = "The Only Warp In Colossus";
 	bitmap portrait;
 	ProgressBar bar;
 	rect dim;
 }
 
-struct Screen {
+struct Screen
+{
 	int w, h;
 	int nEntryZones; // warp zones to other screens
 }
 
-class MapHandler {
+class MapHandler
+{
 	Screen[] screens;
 
-	void loadMapConfig(string filepath = "map.toml") {
+	void loadMapConfig(string filepath = "map.toml")
+	{
 		auto data = parseTOML(cast(string) read(filepath));
 		writefln("file [%s]=[%s]", filepath, data);
 		string name = data["general"]["name"].str;
 		long numberScreens = data["general"]["screens"].integer;
 
-		for (int i = 0; i < numberScreens; i++) {
+		for (int i = 0; i < numberScreens; i++)
+		{
 			auto d = data["screen%u".format(i)];
 			writeln("\t", d);
 			Screen s = Screen(cast(int) d["width"].integer, cast(int) d["height"].integer, cast(int) d["nEntryZones"]
@@ -535,52 +616,61 @@ class MapHandler {
 	void onDraw(Viewport v) => noop;
 }
 
-class World {
+class World
+{
 	MapHandler map;
 	Unit[] objects; // stored per screen, or here and list the screen?
 	Unit[] players; // short-list for players for enumerating just them
 	Item[] items; // items in world
 
-	this(){
+	this()
+	{
 		loadEntityConfig();
 		map.loadMapConfig();
-		}
+	}
 
-	void warpToScreen(Unit thing, int newScreenIndex, pair newPosition) {
+	void warpToScreen(Unit thing, int newScreenIndex, pair newPosition)
+	{
 		thing.physics.screenIndex = newScreenIndex;
 		thing.physics.pos = newPosition;
 	}
 
-	void warpToScreen(ItemInstance thing, int newScreenIndex, pair newPosition) { // is it possible to warp items? Perhaps for inventory systems, exhaust chutes, etc.
+	void warpToScreen(ItemInstance thing, int newScreenIndex, pair newPosition)
+	{ // is it possible to warp items? Perhaps for inventory systems, exhaust chutes, etc.
 		thing.screenIndex = newScreenIndex;
 		thing.pos = newPosition;
 	}
 
-	void onTick() {
+	void onTick()
+	{
 		foreach (o; objects)
 			o.onTick();
 		foreach (i; items)
 			i.onTick();
 	}
 
-	void onDraw(Viewport v) {
+	void onDraw(Viewport v)
+	{
 		foreach (o; objects)
 			o.onDraw(v);
 		foreach (i; items)
 			i.onDraw(v);
 	}
 
-	final void loadEntityConfig(string filepath = "entity.toml") {
+	final void loadEntityConfig(string filepath = "entity.toml")
+	{
 		auto data = parseTOML(cast(string) read(filepath));
 		writefln("file [%s]=[%s]", filepath, data);
 
 		long numberEntities = data["general"]["numberEntities"].integer;
 
-		struct entity {
+		struct entity
+		{
 			string name;
 		}
 
-		for (int i = 0; i < numberEntities; i++) {
+		for (int i = 0; i < numberEntities; i++)
+		{
 			auto d = data["entity%u".format(i)];
 			writeln("\t", d);
 			entity e = entity(d["name"].str);
@@ -589,7 +679,136 @@ class World {
 	}
 }
 
-int main() {
+// or you know, figure out components to describe charactoristics.
+// a door is a door, but also electrical
+// call these [structures]?
+class ElectronicItem : Item
+{
+}
+
+class Switch : ElectronicItem
+{
+}
+
+class Light : ElectronicItem
+{
+}
+
+class Heater : ElectronicItem
+{
+}
+
+class Door : ElectronicItem
+{
+}
+
+class Hatch : Door
+{
+}
+
+// design-by-introspection / whatever
+// electronics try to connect, if (elec is null), 
+// obviously no conect. But there may be other electronics
+// stuff like shock defense in other comps.
+
+struct BodyHealth
+{
+	float leftLeg;
+	float rightLeg;
+	float leftArm;
+	float rightArm;
+	float torso;
+	float groin;
+	float head;
+	float tail;
+	float eyes; // ears?
+} // any other sub-hardpoints? Also what about mutations like 
+// addiontal limbs/etc? special organs? dogs have tail.
+// also issue with sub-hardpoints shouldn't keep you alive.
+// you cannot be a walking/floating pair of eyes with everything else dead.
+
+/// these can be 0-1.0 resistance coefficients, OR, 0 to [whatever] damage values. Same struct so the categories easily line up.
+struct DamageTypes
+{ // we may be WAY OVER customizing, this is for brainstorming possibilities
+	// this isn't a turn based game so good luck integrating tons of these while keeping action going.
+	// as for medical / healing, that can have more complexity.
+	// though how do we handle death? If you can just res, does dying matter?
+	// - Lose your stuff and ???
+	// now losing resources is a classic but it has a problem. The more you lose, the worse off you are. And if you're winning it's meaningless. Is there a better way to disincentivize dying without penalizing the total run? A short term disadvantage? Because if a player is doing too good, they would WANT it to be harder to be more engaging.
+
+	/// daamage to, or res coef for:
+	// internal:
+	float organ;
+	float skin;
+
+	// mostly melee
+	float piercing;
+	float blunt;
+	float slashing;
+
+	// mostly ranged weapons
+	float kinetic; // 
+	float kineticAP; // how to handle armor?
+	float explosion; // HE gun
+	float plasma; //scifi gun
+	float laser; //scifi gun
+	float photon; //scifi gun
+	float phonon; //scifi gun
+
+	// mostly scifi
+	float psi; // mental damage?
+	float force; // force pushing?
+
+	// mostly environmental
+	float acoustic; /// monster sensitive to loud noises?
+	float brightLight; /// monster sensitive to bright lights?
+	float lowLight; /// ability to see in low-light condition
+	float electrical; /// electrical resistance coefficent
+	float fire;
+	float freeze;
+	float vacuum;
+	float highPressure; /// resistance to high pressure damage
+	float extremePressure; /// for something that can survive high pressure but not a higher category.
+	float blister; /// maybe?
+	float steam; /// ? heat?
+	float liquid; /// any liquid. water, etc.
+	float acid; /// caustic environments
+	// any space station 13 ones? etc?
+}
+
+class CombatCom
+{ /// stats. resistances. etc
+
+	//float health; // is there a body health or just sum of all body-parts and if a body part is 'dead' damage is redirected to a living part? Abiotic does this I think.
+	BodyHealth health;
+
+	// instead of "X PSI hurts you" or something like that. we'll
+	// we'll do category/card damage.
+	// even if we have pressure we do category
+	// damage like "high pressure" "very high pressure" "extreme pressure"
+	// because the categories are all that matter mechanically
+	// we're not simulating a full physics system
+	// and even where we do track say, boiler pressur
+	// we quantize it at the end to determine damage category
+}
+
+class ElectronicalCom
+{
+} /// stuff that connects using wires
+class HeatCom
+{
+}
+
+class LiquidCom
+{
+}
+
+class GasCom
+{
+} // vacuum, etc
+
+int main()
+{
 	PixelMap pm;
 	PixelMapCollider pmc = new PixelMapCollider(&pm);
 
